3e529f2a7589962b1e25b3b12b29bad3ff83f720
Prashant Singh <singh123singhprashant@gmail.com>
2022-09-06 20:33:02 +0530

    Interview

---
+++ JavaScript/common.js
@@ -0,0 +1,13 @@
+let s="prashant"
+let s1="prahtsdf"
+let s2=[]
+for(let i=0; i<=s.length-1;i++){
+    for(let j=0; j<=s1.length-1;j++){
+        if(s[i]==s1[j]){
+            console.log(s2[i])
+        }
+        return s2[i]
+    }
+
+
+}
---
+++ JavaScript/string.js
@@ -0,0 +1,9 @@
+function reverse(s){
+    
+    let  s1=[]
+    for(let i=s.length-1;i>=0;i--){
+        s1 = s1+s[i]
+    }
+    return s[i]
+}
+console.log(reverse('yahsuj'))
